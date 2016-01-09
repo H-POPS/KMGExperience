8da2fecc431639cbd9de2bca9cdac9be9e545515
robotman2000 <henricopops@gmail.com>
2016-01-09 19:56:07 +0100

    First

---
+++ CustomFlatRide/FlatRideLoader.cs
@@ -0,0 +1,69 @@
+﻿
+using System.Collections.Generic;
+using UnityEngine;
+public class FlatRidesLoader : CustomFlatRideLoader
+{
+
+    public void LoadKMG()
+    {
+        GameObject asset = base.LoadAsset("KMGExperience");
+        List<Transform> Arms = new List<Transform>();
+        List<Transform> ArmRotators = new List<Transform>();
+        List<Transform> Main = new List<Transform>();
+        List<Transform> RotateAxis = new List<Transform>();
+        Utility.recursiveFindTransformsStartingWith("Arm_", asset.transform, Arms);
+        Utility.recursiveFindTransformsStartingWith("ArmRota", asset.transform, ArmRotators);
+        Utility.recursiveFindTransformsStartingWith("MainRotator", asset.transform, Main);
+        Utility.recursiveFindTransformsStartingWith("RotateAxis", asset.transform, RotateAxis);
+        KMGExperience KMGE = asset.AddComponent<KMGExperience>();
+        KMGE.armRaiseAxis = Arms;
+        KMGE.armSpinAxis = ArmRotators;
+        KMGE.armAxis = RotateAxis;
+        KMGE.mainAxis = Main[0];
+        base.AddColors(asset, new Color[2]{ base.ConvertColor(171, 18, 16), Color.yellow });
+        base.AddRestraints(asset, new Vector3(0, 0, 120));
+        base.BasicFlatRideSettings(KMGE,"KMG Expirence", 800, .7f, .4f, .6f, 6, 6);
+        List<int> connections = new List<int>()
+        {
+            0,22,
+            22,1,
+            1,21,
+            21,2,
+            20,2,
+            20,3,
+            3,19,
+            19,4,
+            4,18,
+            18,5,
+            5,0,
+            20,7,
+            20,14,
+            18,8,
+            18,17,
+            22,11,
+            22,12,
+            4,16,
+            16,17,
+            5,9,
+            9,8,
+            0,11,
+            11,10,
+            1,13,
+            13,12,
+            2,15,
+            15,14,
+            18,20,
+            18,22,
+            22,20
+
+        };
+        base.SetWaypoints(asset, connections);
+    }
+    public void LoadFlatRides()
+    {
+        LoadKMG();
+    }
+
+}
+
+
---
+++ CustomFlatRide/FlatRideScript/KMGExperience.cs
@@ -0,0 +1,125 @@
+﻿
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KMGExperience : FlatRide
+{
+    private new enum State
+    {
+        STARTING,
+        RUNNING,
+        LOWERING,
+        STOPPING
+    }
+
+    public Transform mainAxis;
+
+    public List<Transform> armRaiseAxis;
+
+    public List<Transform> armSpinAxis;
+
+    public List<Transform> armAxis;
+    public int spins = 3;
+
+    public FabricEvent raiseArmSound;
+
+    public FabricEvent lowerArmSound;
+
+    private Quaternion armTargetRotation = Quaternion.Euler(50f, 0f, 0f);
+
+    private int accelerationSpeed = 122;
+
+    private float maxSpeed = 60f;
+
+    private float armSpinAxisMaxSpeed = 70f;
+
+    [Serialized]
+    private KMGExperience.State currentState;
+
+    [Serialized]
+    private Rotator mainRotator = new Rotator();
+
+    [Serialized]
+    private Rotator armSpinRotator = new Rotator();
+
+    [Serialized]
+    private RotateBetween armRaise = new RotateBetween();
+
+    public override void Start()
+    {
+        base.Start();
+        this.armRaise.Initialize(this.armRaiseAxis[0], Quaternion.identity, this.armTargetRotation, 4f);
+        this.mainRotator.Initialize(this.mainAxis, (float)this.accelerationSpeed, this.maxSpeed);
+        this.armSpinRotator.Initialize(this.armSpinAxis[0], (float)this.accelerationSpeed, this.armSpinAxisMaxSpeed);
+        this.armSpinRotator.setDirection(-1);
+    }
+
+    public override void onStartRide()
+    {
+        base.onStartRide();
+        this.currentState = KMGExperience.State.STARTING;
+        this.mainRotator.start();
+    }
+
+    public override void tick(StationController stationController)
+    {
+        if (stationController != null && stationController.getState() != StationController.State.ACTIVE)
+        {
+            return;
+        }
+        this.armRaise.tick(Time.deltaTime);
+        this.mainRotator.tick(Time.deltaTime);
+        this.armSpinRotator.tick(Time.deltaTime);
+        if (this.currentState != KMGExperience.State.LOWERING && this.currentState != KMGExperience.State.STOPPING)
+        {
+            if (this.mainRotator.getCurrentSpeed() > this.mainRotator.getMaxSpeed() * 0.8f && this.armRaise.startFromTo())
+            {
+                Fabric.EventManager.Instance.PostEvent(this.raiseArmSound.name, base.gameObject);
+            }
+        }
+        else if (this.armSpinRotator.getCurrentSpeed() < this.armSpinRotator.getMaxSpeed() * 0.2f && this.armRaise.startToFrom())
+        {
+            Fabric.EventManager.Instance.PostEvent(this.lowerArmSound.name, base.gameObject);
+        }
+        if (this.currentState == KMGExperience.State.STARTING)
+        {
+            if (this.mainRotator.reachedFullSpeed())
+            {
+                this.armSpinRotator.start();
+                this.currentState = KMGExperience.State.RUNNING;
+                base.triggerRunloopSound();
+            }
+        }
+        else if (this.currentState == KMGExperience.State.RUNNING)
+        {
+            if (this.armSpinRotator.getCompletedRotationsCount() >= this.spins)
+            {
+                this.armSpinRotator.stop();
+                this.currentState = KMGExperience.State.LOWERING;
+            }
+        }
+        else if (this.currentState == KMGExperience.State.LOWERING && this.armSpinRotator.isStopped())
+        {
+            this.currentState = KMGExperience.State.STOPPING;
+            this.mainRotator.stop();
+            base.triggerDecelerateSound();
+        }
+        foreach(Transform T in armRaiseAxis)
+        {
+            T.localRotation = armRaiseAxis[0].localRotation;
+        }
+        foreach(Transform T in armSpinAxis)
+        {
+            T.localRotation = armSpinAxis[0].localRotation;
+        }
+        foreach (Transform T in armAxis)
+        {
+            T.localRotation = armSpinAxis[0].localRotation;
+        }
+    }
+
+    public override bool shouldLetGuestsOut()
+    {
+        return this.currentState == KMGExperience.State.STOPPING && this.mainRotator.isStopped();
+    }
+}
